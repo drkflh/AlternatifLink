ae467079268a8300a6c3a9edb09e789a028ed10e
darik aflah <87356765+drkflh@users.noreply.github.com>
2021-11-13 18:10:33 +0700

    Add files via upload

---
+++ src/assets/items.js
@@ -0,0 +1,38 @@
+import website from '../assets/website.png'
+import instagram from '../assets/instagram.png'
+import dribbble from '../assets/dribbble.png'
+import github from '../assets/github.png'
+import linkedin from '../assets/linked.png'
+
+const items = [{
+    "title": "Website",
+    "subtitle": "@Darik Aflah A.A | Web Portofolio ",
+    "image": website,
+    "link": "https://drkflh.github.io/" //your personal website or portfolio  link
+},
+{
+    "title": "GitHub",
+    "subtitle": "@drkflh | Tempatnya Source Code Nich ",
+    "image": github,
+    "link": "https://github.com/drkflh" //Github Profile link
+},
+{
+    "title": "Instagram",
+    "subtitle": "@drkflh | Kegiatan Sehari - Hari ",
+    "image": instagram,
+    "link": "https://www.instagram.com/drkflh/" //instagram profile link 
+},
+{
+    "title": "Dribbble",
+    "subtitle": "@Darik Aflah | Desain - Desain Aplikasi ",
+    "image": dribbble,
+    "link": "https://dribbble.com/drkflh" // Dribbble profile link 
+},
+{
+    "title": "LinkedIn",
+    "subtitle": "@Darik Aflah | Rekrut Disini Cuy ",
+    "image": linkedin,
+    "link": "https://www.linkedin.com/in/darik-aflah-480b61219/"//youtube channel link 
+}]
+
+export default items
